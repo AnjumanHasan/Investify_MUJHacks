a12c62ccb719e08e347c095219f32e09303d55df
Anjuman Hasan <anju2084@gmail.com>
2022-10-01 18:53:33 +0530

    Added acitivities

---
+++ app/src/main/java/thoseITgirls/investify/HangOn.kt
@@ -0,0 +1,11 @@
+package thoseITgirls.investify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HangOn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hang_on)
+    }
+}
